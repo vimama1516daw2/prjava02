162c3cdc02534cbb61afd8e8bb1c13dbcb03ec98
victor <vimama1516daw2@gmail.com>
2016-04-08 20:07:36 +0200

    Versió 0.1 del projecte prjava00

---
+++ prjava00/src/Common/Test.java
@@ -0,0 +1,10 @@
+package Common;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hola món");
+		System.out.println("Versió 0.1 del projecte prjava00");
+	}
+
+}
