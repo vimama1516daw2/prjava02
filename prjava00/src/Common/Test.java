681cfe260b1443259238a88451d81a5d87aeb20d
victor <vimama1516daw2@gmail.com>
2016-04-11 18:02:44 +0200

    Commit nº2.

--- prjava00/src/Common/Test.java
+++ prjava00/src/Common/Test.java
@@ -1,10 +1,27 @@
 package Common;
 
+import java.net.InetAddress;
+import java.net.*;
+
 public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hola món");
-		System.out.println("Versió 0.1 del projecte prjava00");
+		System.out.println("Versió 0.2 del projecte prjava00");
+		try{
+			InetAddress addr = InetAddress.getLocalHost();
+			String ipAddr = addr.getHostAddress();
+			String hostname = addr.getHostName();
+			System.out.println("hostname="+hostname);
+			System.out.println("Adreça IP:" + ipAddr);
+			System.out.println("Nom de l'usuari: " + System.getProperty("user.name"));
+			System.out.println("Carpeta Personal: " + System.getProperty("user.home"));
+			System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
+			System.out.println("Versió OS: " + System.getProperty("os.version"));
+		}
+		catch (UnknownHostException e){
+			e.printStackTrace();
+		}
 	}
 
 }
